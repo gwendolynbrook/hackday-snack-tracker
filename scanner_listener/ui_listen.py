05ce705952fcdaf185ce20d817eccee4a4d8de0e
Gwen Johnson <gwendolynbrook@gmail.com>
2018-04-13 16:50:56 -0500

    hack local host

--- scanner_listener/ui_listen.py
+++ scanner_listener/ui_listen.py
@@ -11,14 +11,16 @@ from functools import partial
 env = partial(os.environ.get)
 
 class Config:
-    BARCODE_INPUT_DEVICE = env("BARCODE_INPUT_DEVICE")
-    SNACK_SERVICE_HOST = "http://localhost:8080" #env("SNACK_SERVICE_HOST")
+    SNACK_SERVICE_HOST = env("SNACK_SERVICE_HOST")
+    SNACK_SERVICE_HOST_DEFAULT = "http://localhost:8080"
 
 class BarcodeReader():
     def __init__(self, config):
         self.barcode_reader = None
+        self.barcode_reader_addr = None
         self.config = config
         self.code = ''
+        self.find_barcode_reader()
         # Try to connect
         self.connect_to_reader()
 
@@ -26,16 +28,32 @@ class BarcodeReader():
         print("Posting : {}".format(code))
         data = {"item_code": code}
         headers = {"content-type": "application/json"}
+        host = self.config.SNACK_SERVICE_HOST if self.config.SNACK_SERVICE_HOST \
+            else self.config.SNACK_SERVICE_HOST_DEFAULT
+        print(host)
         try:
-            r = requests.post(self.config.SNACK_SERVICE_HOST + "/state/item_code", data=json.dumps(data), headers=headers)
+            r = requests.post(host + "/state/item_code", data=json.dumps(data), headers=headers)
             print(r.content)
         except requests.exceptions.ConnectionError as e:
             print("Failed to connect. Please try again.")
 
+    def find_barcode_reader(self):
+        print("Listing devices.")
+        devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+
+        for device in devices:
+            print(device.fn, device.name, device.phys)
+            if("Barcode Reader" in device.name):
+                self.barcode_reader_addr = device.fn
+                break
+
     def connect_to_reader(self):
+        self.barcode_reader_addr = None
         self.barcode_reader = None
         try:
-            self.barcode_reader = evdev.InputDevice('/dev/input/event17')
+            self.find_barcode_reader()
+            if self.barcode_reader_addr is not None:
+                self.barcode_reader = evdev.InputDevice(self.barcode_reader_addr)
         except OSError as e:
             print("No barcode reader found: {0}".format(e))
 
